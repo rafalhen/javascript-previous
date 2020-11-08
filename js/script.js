979af3ad7a60ea2287b6f50faa50edf3f4694006
Rafal Hen <rafalhen@poczta.onet.pl>
2020-11-08 17:52:49 +0100

    wersjajava2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let computerMove = ` kamień`;
+
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = ' papier';
+
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
